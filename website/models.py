46cc9936ec52a87ade77630b29c550ed584fc913
theorivero <thrivero1@gmail.com>
2021-04-30 14:48:47 -0300

    dinamic menu

--- website/models.py
+++ website/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class itemGroup(models.Model):
+	name = models.CharField(max_length=200, null=True)
+
+	def __str__(self):
+		return self.name
+
+class item(models.Model):
+	name = models.CharField(max_length= 200, null=True)
+	price = models.CharField(max_length= 200, null=True)
+	description = models.CharField(max_length= 200, null=True)
+	group = models.ForeignKey(itemGroup, null=True, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.name
--- website/views.py
+++ website/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
+from .models import *
+
 
 def index(request):
-	context = {}
+	itemGroups = itemGroup.objects.all()
+	context = {'itemGroups': itemGroups}
 
 	return render(request, 'index.html', context)
